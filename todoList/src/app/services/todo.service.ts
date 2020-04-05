eedd5b540c74d099a29069a1a2a330efbea55f1c
Md Kawsar Hussen <kawsarhussen@gmail.com>
2020-04-05 17:57:01 -0400

    "Add and Header  implemented"

--- todoList/src/app/services/todo.service.ts
+++ todoList/src/app/services/todo.service.ts
@@ -25,4 +25,14 @@ export class TodoService {
     const url = `${this.todosUrl}/${todo.id}`;
     return this.http.put(url, todo, httpOptions);
   }
+  /// delete Completed
+  deleteTodo(todo: Todo): Observable<Todo> {
+    const url = `${this.todosUrl}/${todo.id}`;
+    return this.http.delete<Todo>(url, httpOptions);
+  }
+
+  //Add todo
+  addTodo(todo: Todo): Observable<Todo> {
+    return this.http.post<Todo>(this.todosUrl, todo, httpOptions);
+  }
 }
